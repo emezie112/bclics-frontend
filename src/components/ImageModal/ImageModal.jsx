93a96f9df4c8d0d371530bae61b6356417d706ba
emma-junior <emnzboy31@gmail.com>
2023-04-07 21:30:30 +0100

    fixed minor bug in img modal component

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./imageModal.css"
 import { PostsData } from '../../Data/PostsData'
+import times from "../Icons/times.svg"
 
 const ImageModal = ({imageNum, setImgModal, imgModal}) => {
   return (
     <div className='pic-wrapper'>
-        <h2 onClick={() => setImgModal(!imgModal)}>X</h2>
+        <div className='close-icon-wrapper' onClick={() => setImgModal(!imgModal)}>
+          <img src={times} alt='close-btn' />
+        </div>
         <div className='picc'><img src={PostsData[imageNum].img} alt='pic' /></div>
     </div>
   )
